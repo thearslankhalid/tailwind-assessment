21a972ad52e9b010e058fee801e8b4f67e0d4c55
ak8741096 <ak8741096@gmail.com>
2020-07-04 10:32:00 +0500

    Added responsiveness + changed the icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+var colors = {
+  'm-primary': '#141738',
+  'm-secondary': '#3E458D',
+  'm-tertiary': '#7B6AB5',
+  'm-text': '#E8E4F5',
+  'm-bg': '#FFF'
+}
+
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: colors,
+      textColors: colors,
+      backgroundColors: colors
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+      require('@tailwindcss/custom-forms'),
+  ],
 }
